707c6728c0b3d3cd5d8a06badc119cdcdf15893b
won <wjin@nyit.edu>
2014-06-13 03:50:49 -0400

    trying to get fb login to work

--- js/facebook/fs.js
+++ js/facebook/fs.js
@@ -63,7 +63,7 @@
       //submit form after 1.5 seconds (show user their e-mail before sending)
       setTimeout(function(){
 
-          $('#sendBut').click();
+          $('#sendBut1').click();
       }, 1500);
     
 
